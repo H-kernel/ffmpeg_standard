3405c0cdef5cfcfca3fd32ac07fc406c8ded159b
timehunter <shrong.he@gmail.com>
2021-02-01 12:12:20 +0800

    ios build

--- build/build-ios.sh
+++ build/build-ios.sh
@@ -266,7 +266,7 @@ build_x264()
 	        HOST="--host=arm-apple-darwin"
 			XARCH="-arch arm"
 	    fi
-        CFLAGS="$CFLAGS -fembed-bitcode -mios-version-min=11.3"
+        CFLAGS="$CFLAGS -fembed-bitcode -mios-version-min=11.3 -all_load"
         ASFLAGS="$CFLAGS"
 	fi
 
